dd821fb6d8c02ab0d747ee2f35ba7855d9dab68a
Agrim Gupta <agrimg8@gmail.com>
2025-09-13 18:35:24 +0000

    Variables

---
+++ basic_01/variable.js
@@ -0,0 +1,12 @@
+const accountId = 32134
+let accountEmail = "agrim@gmail.com"
+var accoundPassword = "345672" //Not preferred to used becoz 'var' dont know its block scope
+accountCity = "Ghaziabad"
+
+// accountId = 2  //not allowed to change 'const' variable
+accountEmail = "dssd@ds.com"
+accountPassword = "332233"
+accountCity = "jsja"
+
+console.table([accountId, accountEmail, accoundPassword, accountCity]);
+
