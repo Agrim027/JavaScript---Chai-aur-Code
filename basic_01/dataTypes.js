655868455c0f09b3b70d9d8757f5177bdac650a7
Agrim Gupta <agrimg8@gmail.com>
2025-09-13 18:34:59 +0000

    dataTypes

---
+++ basic_01/dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // Treat all JS code as newer version
+
+console.log(3+3)
+
+let name = "Agrim"
+let age = 21
+let isLoggedIn = false 
+
+//DataTypes-->
+
+//number=>2 to power 53
+//bigInt
+//String = ""
+//boolean = true/false
+// null = stand alone value
+// undefined 
+// symbol = unique
+
+//object 
+
+console.log(typeof name);       // string
+console.log(typeof null);       // object
+console.log(typeof undefined);  // undefined
+
