f68137bc3322c21c721853ae2fdb605e0dd666b5
Agrim Gupta <agrimg8@gmail.com>
2025-09-14 08:29:42 +0000

    Operations

--- basic_01/Conversion_Operation.js
+++ basic_01/Conversion_Operation.js
@@ -1,10 +1,25 @@
-let score = "33"   //e.g null = 0
+// let score = "33"   //e.g null = 0
 
-console.log(typeof score);
+// console.log(typeof score);
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
 
-//"33"=>33
-// "33abc" => NaN
-// true => 1; false => 0
+// // "33"=>33
+// // "33abc" => NaN
+// // true => 1; false => 0
+
+
+//  *********** Operations *********
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Agrim"
+console.log(str1+str2);
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
