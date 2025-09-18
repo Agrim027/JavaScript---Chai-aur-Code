c12f640f0846bd208d708f46efbca72484fccebf
Agrim Gupta <agrimg8@gmail.com>
2025-09-18 05:14:45 +0000

    String

---
+++ basic_02/01_String.js
@@ -0,0 +1,35 @@
+// const name = "Agrim "
+// const repCount = 13 
+
+// console.log(name + repCount + " Value");
+
+// console.log(`My name is ${name} and my repo count is ${repCount}`);
+
+const gameName = new String('Agrim')
+// String {'Agrim'}
+// 0: "A"
+// 1: "g"
+// 2: "r"
+// 3: "i" 
+// 4: "m"
+// length: 5
+
+// console.log(gameName[0]); 
+// console.log(gameName.length);
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('A'));
+// console.log(gameName.substring(0,2));
+// console.log(gameName.slice(-6,4));
+
+// const name = "      Agrim       "
+// console.log(name);
+// console.log(name.trim());
+
+const url  = "https://agrim.com/agrim6383"
+// console.log(url.replace('6383', '027'));
+// console.log(url.includes('6383'));
+
+console.log(url.split('/'));  //split based on '/'
+//you can do it on ' ' or '-' or anything else
