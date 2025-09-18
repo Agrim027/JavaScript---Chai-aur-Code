18025179b98f22bb97e6b4fc1ee074f1ec3d0aa0
Agrim Gupta <agrimg8@gmail.com>
2025-09-18 04:06:33 +0000

    done

--- basic_01/dataTypes.js
+++ basic_01/dataTypes.js
@@ -1,6 +1,6 @@
 "use strict"; // Treat all JS code as newer version
 
-console.log(3+3)
+// console.log(3+3);
 
 let name = "Agrim"
 let age = 21
@@ -8,17 +8,41 @@ let isLoggedIn = false
 
 //DataTypes-->
 
-//number=>2 to power 53
-//bigInt
-//String = ""
-//boolean = true/false
-// null = stand alone value
-// undefined 
-// symbol = unique
+//Primimtive
 
-//object 
+//1.number  => 2 to power 53
+//2.bigInt
+//3.String = ""
+//4.boolean = true/false
+//5.null = stand alone value
+//6.undefined                    let userMail;
+//7.symbol = unique
 
-console.log(typeof name);       // string
-console.log(typeof null);       // object
-console.log(typeof undefined);  // undefined
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId);
 
+
+// console.log(typeof name);       // string
+// console.log(typeof null);       // object
+// console.log(typeof undefined);  // undefined
+
+
+
+//Reference(Non-Primitve)
+
+//Array, Object, Functions
+
+const heros = ["IronMan", "Dr.Strange", "Thor"];
+
+let myObj ={
+    name : "Agrim",
+    age: 22,
+}
+
+const myFunct = function(){
+    console.log("Hello World");
+    
+}
+
+console.log(typeof heros);
